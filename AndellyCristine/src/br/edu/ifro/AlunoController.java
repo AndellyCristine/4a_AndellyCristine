194bcb53c36800de53ad428d0a90391457e1e1f6
04380230210 <04380230210@JIPA-LABIN05-14.ifro.local>
2018-05-11 08:57:11 -0400

    Gravar no banco de dados a partir da tela de cadastro URL feito

--- AndellyCristine/src/br/edu/ifro/AlunoController.java
+++ AndellyCristine/src/br/edu/ifro/AlunoController.java
@@ -5,9 +5,19 @@
  */
 package br.edu.ifro;
 
+import br.edu.ifro.modelo.Aluno;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 /**
  * FXML Controller class
@@ -16,12 +26,38 @@ import javafx.fxml.Initializable;
  */
 public class AlunoController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private TextField txtNome;
+    @FXML
+    private Label lblNome;
+    @FXML
+    private Button btnSalvar;
+    @FXML
+    private Button btnFechar;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
+    }
+        
+        
+    private void Salvar(ActionEvent event){
+    EntityManagerFactory emf  = Persistence.createEntityManagerFactory("aula");
+    EntityManager em = emf.createEntityManager();
+    
+    Aluno aluno1 = new Aluno();
+    aluno1.setNome(txtNome.getText());
+    
+    em.getTransaction().begin();
+    
+    em.persist(aluno1);
+    
+    em.getTransaction().commit();
+    
+    }
+    private void fechar(ActionEvent event){
+        Stage stage = (Stage) txtNome.getScene().getWindow();
+        stage.close();
+    }
     }    
     
-}
